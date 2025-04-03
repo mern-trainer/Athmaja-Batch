4d6799ec819f613cf8679ec0cae824b1145e835a
VISHNU M K <mail.vishnumk@gmail.com>
2025-04-03 15:39:18 +0530

    react basics

---
+++ ReactJS/react_app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const App = () => {
+    return <React.Fragment>
+        <div>
+            <h1>Hi</h1>
+            <p>Hello</p>
+        </div>
+        <div>
+            <h3>Hello</h3>
+        </div>
+    </React.Fragment>
+}
+
+export default App
