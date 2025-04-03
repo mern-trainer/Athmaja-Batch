d3415df369db2f68f889d3e1551aba6d22874181
VISHNU M K <mail.vishnumk@gmail.com>
2025-04-03 16:13:50 +0530

    click event

--- ReactJS/react_app/src/App.jsx
+++ ReactJS/react_app/src/App.jsx
@@ -1,14 +1,25 @@
 import React from "react"
 
 const App = () => {
+
+    const number = 1000
+
+    const handleClick = (text) => {
+        console.log(text);
+    }
+
     return <React.Fragment>
         <div>
             <h1>Hi</h1>
             <p>Hello</p>
         </div>
+        <div>Age is: {number}</div>
         <div>
             <h3>Hello</h3>
         </div>
+        <button onClick={() => {
+            handleClick("button clicked")
+        }}>Click Here</button>
     </React.Fragment>
 }
 
---
+++ ReactJS/react_app/src/Pages/PasswordGenerator.jsx
@@ -0,0 +1,7 @@
+const PasswordGenerator = () => {
+    return <div>
+        <h1>Password Generator</h1>
+    </div>
+}
+
+export default PasswordGenerator
