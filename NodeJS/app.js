1e22faff9c9e0fb3455bc8ced5467039783bc521
mern <mail.vishnumk@gmail.com>
2025-04-26 15:30:10 +0530

    streams

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -6,33 +6,59 @@
 
 const fs = require("fs");
 
-fs.writeFile("sample.txt", "Hello World", (err) => {
-    if (err) {
-        return console.log(err.message);
-    }
-    console.log("File Created");
+// fs.writeFile("sample.txt", "Hello World", (err) => {
+//     if (err) {
+//         return console.log(err.message);
+//     }
+//     console.log("File Created");
+// })
+
+// fs.appendFile("sample.txt", "\nAppend File", (err) => {
+//     if (err) {
+//         return console.log(err.message);
+//     }
+//     console.log("File updated");
+// })
+
+// fs.readFile("sample.txt", { encoding: "utf-8" }, (err, data) => {
+//     if (err) {
+//         return console.log(err.message);
+//     }
+//     console.log(data);
+// })
+
+// fs.unlink("sample.txt", (err) => {
+//     if(err){
+//         return console.log(err.message);
+//     }
+//     console.log("File Deleted");
+// })
+
+// fs.writeFileSync("sample.txt", "Hello World");
+
+// const res = fs.readFileSync("sample.txt", { encoding: "utf-8" });
+
+// console.log(res);
+
+
+//  STREAMS -> Read, write
+
+// / 1GB, 10GB,
+
+// movies -> 2MB -> 1GB -> 1024MB -> 512s -> 8 - 9min
+// streams -> small files 16kb =>
+
+const readStream = fs.createReadStream("streams.txt", { encoding: "utf-8" });
+const writeStream = fs.createWriteStream("downloaded_stream.txt", { encoding: "utf-8" });
+
+readStream.on("data", (chunk) => {
+    writeStream.write(`CHUNK: ${chunk}`);
 })
 
-fs.appendFile("sample.txt", "\nAppend File", (err) => {
-    if (err) {
-        return console.log(err.message);
-    }
-    console.log("File updated");
+readStream.on("error", (err) => {
+    console.log(err.message);
 })
 
-fs.readFile("sample.txt", { encoding: "utf-8" }, (err, data) => {
-    if (err) {
-        return console.log(err.message);
-    }
-    console.log(data);
+readStream.on("end", () => {
+    console.log("Completed");
 })
-
-fs.unlink("sample.txt", (err) => {
-    if(err){
-        return console.log(err.message);
-    }
-    console.log("File Deleted");
-})
-
-
-
